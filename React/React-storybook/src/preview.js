19bb6a650d6cc4da241f1b3d8b7cfa7c1b191330
kiki <amyvsvivi@gmail.com>
2019-02-20 01:52:40 +0800

    Update ReactStorybook

--- React/React-storybook/src/preview.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-class Preview extends Component {
-    render() {
-        return <p>qdwasdd</p>;
-    }
-}
-
-ReactDOM.render(<Preview />, document.getElementById('root'));
